17eaabc92441ad76083d7313d3195c50cf943895
LouisPaskalisGinting <118007462+LouisPaskalisGinting@users.noreply.github.com>
2023-12-18 10:49:17 +0700

    proses-ubah.php

--- UAS_121140066_Louis Paskalis Ginting/proses-ubah.php
+++ UAS_121140066_Louis Paskalis Ginting/proses-ubah.php
@@ -1,14 +1,5 @@
- <!-- Aplikasi CRUD
- ************************************************
- * Developer    : Indra Styawantoro
- * Company      : Indra Studio
- * Release Date : 1 Maret 2016
- * Website      : http://www.indrasatya.com, http://www.kulikoding.net
- * E-mail       : indra.setyawantoro@gmail.com
- * Phone        : +62-856-6991-9769
- * BBM          : 7679B9D9
- -->
- 
+--Louis Paskalis Ginting--
+--121140066--
 <?php
 // memanggil file siswa.php
 require_once 'siswa.php';
